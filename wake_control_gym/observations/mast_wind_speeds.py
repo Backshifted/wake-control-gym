5dca920880c9f6d08943d1271838e1afe5ba8575
Onno Verberne <backshifted@gmail.com>
2024-09-30 00:36:40 +0200

    Bugfix for mast wind speeds

--- wake_control_gym/observations/mast_wind_speeds.py
+++ wake_control_gym/observations/mast_wind_speeds.py
@@ -13,10 +13,10 @@ class MastWindSpeeds(Observation):
     def __init__(self, simulator: Simulator, measurement_points: list[MeasurementPoint]) -> None:
         self.dim = len(measurement_points)
         self.low = torch.full(
-            (simulator.num_turbines,), MIN_WINDSPEED, device=simulator.device, dtype=simulator.dtype
+            (self.dim,), MIN_WINDSPEED, device=simulator.device, dtype=simulator.dtype
         )
         self.high = torch.full(
-            (simulator.num_turbines,), MAX_WINDSPEED, device=simulator.device, dtype=simulator.dtype
+            (self.dim,), MAX_WINDSPEED, device=simulator.device, dtype=simulator.dtype
         )
         self.measurement_point_indices = simulator.register_measurement_points(measurement_points)
 
